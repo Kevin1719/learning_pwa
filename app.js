7ef5b66c80593a5c109751e179e03df6e8635d15
Kevin1719 <kevin21.aps2b@gmail.com>
2023-10-22 19:42:49 +0300

    show spinner and image

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const container = document.getElementById("root");
 const moviename = document.getElementById("query");
 const search = document.getElementsByClassName("btn")[0];
+const spinner = document.getElementById("spinner");
 
 const url = `https://pokeapi.co/api/v2/pokemon?limit=151`;
 const API_KEY="f91017a8a042da0d3b251a9187da7f97"
@@ -13,6 +14,7 @@ const options = {
 };
 
 search.addEventListener('click', async () => {
+  spinner.style.display = "block"; // Show the spinner
   let output = "";
   if(moviename.value !== ""){
     const data = await fetch(`https://api.themoviedb.org/3/search/movie?query=${moviename.value}&api_key=${API_KEY}`, options)
@@ -22,58 +24,35 @@ search.addEventListener('click', async () => {
       }
       return res.json();
     })
-    data?.results.map((movie) =>
-    (output += `
-      <div class="col">
-        <div class="card">
-          <a class="card-media" href="./img-01.jpeg">
-            <img src="https://image.tmdb.org/t/p/w500${movie?.poster_path}" alt="No image" width="100%" />
-          </a>
-          <div class="card-content">
-            <div class="card-cont-header">
-              <div class="cont-left">
-                <h3 style="font-weight: 600">${movie?.original_title}</h3>
-                <span style="color: #12efec">${movie?.release_date}</span>
+      spinner.style.display = "none"; // Show the spinner
+      data?.results.map((movie) =>
+      (output += `
+        <div class="col">
+          <div class="card">
+            <a class="card-media" href="./img-01.jpeg">
+              <img src="https://image.tmdb.org/t/p/w500${movie?.poster_path}" alt="No image" width="100%" />
+            </a>
+            <div class="card-content">
+              <div class="card-cont-header">
+                <div class="cont-left">
+                  <h3 style="font-weight: 600">${movie?.original_title}</h3>
+                  <span style="color: #12efec">${movie?.release_date}</span>
+                </div>
               </div>
-            </div>
-            <div class="describe">
-              ${movie?.overview}
+              <div class="describe">
+                ${movie?.overview}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    `)
-  );
-    console.log(data.results)
+      `)
+    );
     container.innerHTML = output;
   }
   else{
     alert("Champ de recherche vide")
   }
 })
-
-
-// const showCoffees = async () => {
-//   let output = "";
-//   const data = await fetch(url).then((response) => {
-//     if(!response.ok){
-//       return null;
-//     }
-//     return response.json()
-//   })
-  
-//   data?.results.map((d) =>
-//       (output += `
-//               <div class="card">
-//                 <img class="card--avatar" src=${`https://img.pokemondb.net/artwork/large/${d?.name}.jpg`} />
-//                 <h1 class="card--title">${d?.name}</h1>
-//               </div>
-//               `)
-//   );
-//   container.innerHTML = output;
-// };
-
-// document.addEventListener("DOMContentLoaded", showCoffees);
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
